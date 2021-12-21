5523b9b86d16cf156c2f53eac165146ad78d77bb
NxNghi <xuannghi1223003@gmail.com>
2021-12-21 11:23:44 +0700

    add all file

--- templates-parts/header.php
+++ templates-parts/header.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?php assets('css/styles.css')?>">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
     <title><?php echo isset($page_title)?$page_title:'New Document'?></title>
 </head>
 <body>
